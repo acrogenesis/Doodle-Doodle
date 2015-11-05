e66030eddc193585925b4cc90d97b3964082c8bd
Andres Lopez De Leon <agldeleon@gmail.com>
2015-11-05 10:21:04 -0600

    Add generators

--- generators/javascript/begin_end.js
+++ generators/javascript/begin_end.js
@@ -3,3 +3,10 @@
 goog.provide('Blockly.JavaScript.begin_end');
 
 goog.require('Blockly.JavaScript');
+
+Blockly.JavaScript['begin_end'] = function(block) {
+  var statements_main = Blockly.JavaScript.statementToCode(block, 'main');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  return code;
+};
