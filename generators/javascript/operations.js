579731a5dc712356547c7c58fcd1554698bebeff
Andres Lopez De Leon <agldeleon@gmail.com>
2015-11-15 12:37:46 -0600

    Add substractions, multiplications and divisions

--- generators/javascript/operations.js
+++ generators/javascript/operations.js
@@ -112,28 +112,205 @@ Blockly.JavaScript['sum'] = function(block) {
 };
 
 Blockly.JavaScript['substraction'] = function(block) {
-  var value_left_substractor = Blockly.JavaScript.valueToCode(block, 'left_substractor', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_right_substractor = Blockly.JavaScript.valueToCode(block, 'right_substractor', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
+  var value_left_substractor = Blockly.JavaScript.statementToCode(block, 'left_substractor');
+  var value_right_substractor = Blockly.JavaScript.statementToCode(block, 'right_substractor');
 
-Blockly.JavaScript['division'] = function(block) {
-  var value_nominator = Blockly.JavaScript.valueToCode(block, 'nominator', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_denominator = Blockly.JavaScript.valueToCode(block, 'denominator', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  var left_quadruple = false;
+  var right_quadruple = false;
+
+  var left_value;
+  var left_type;
+  var left_index;
+  var left_var_type;
+
+  var right_value;
+  var right_type;
+  var right_index;
+  var right_var_type;
+
+  if (Number(value_left_substractor)) {
+    left_quadruple = quadruples[Number(value_left_substractor)][3];
+    left_type = indexToType(left_quadruple[0]);
+    left_var_type = left_type;
+  } else {
+    left_value = value_left_substractor.split('†')[0];
+    left_type = value_left_substractor.split('†')[1];
+    left_var_type = left_type;
+  }
+
+  if (Number(value_right_substractor)) {
+    right_quadruple = quadruples[Number(value_right_substractor)][3];
+    right_type = indexToType(right_quadruple[0]);
+    right_var_type = right_type;
+  } else {
+    right_value = value_right_substractor.split('†')[0];
+    right_type = value_right_substractor.split('†')[1];
+    right_var_type = right_type;
+  }
+
+  if (left_type === 'var'){
+    if (findVariable(left_value) === -1){
+      alert('Variable "' + left_value + '" not defined.');
+      throw('Semantic Error');
+    } else {
+      left_index = findVariable(left_value);
+      left_var_type = indexToType(left_index);
+    }
+  } else if(left_quadruple === false) {
+    left_quadruple = left_value;
+  }
+
+  if (right_type === 'var'){
+    if (findVariable(right_value) === -1){
+      alert('Variable "' + right_value + '" not defined.');
+      throw('Semantic Error');
+    } else {
+      right_index = findVariable(right_value);
+      right_var_type = indexToType(right_index);
+    }
+  } else if(right_quadruple === false) {
+    right_quadruple = right_value;
+  }
+
+  var result_quadruple = resultType(left_var_type, right_var_type, 1);
+
+  quadruples.push(['-', left_quadruple, right_quadruple, result_quadruple]);
+
+  return '' + (quadruples.length-1);
 };
 
 Blockly.JavaScript['multiplication'] = function(block) {
-  var value_left_multiplier = Blockly.JavaScript.valueToCode(block, 'left_multiplier', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_right_multiplier = Blockly.JavaScript.valueToCode(block, 'right_multiplier', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  var value_left_multiplier = Blockly.JavaScript.statementToCode(block, 'left_multiplier');
+  var value_right_multiplier = Blockly.JavaScript.statementToCode(block, 'right_multiplier');
+
+  var left_quadruple = false;
+  var right_quadruple = false;
+
+  var left_value;
+  var left_type;
+  var left_index;
+  var left_var_type;
+
+  var right_value;
+  var right_type;
+  var right_index;
+  var right_var_type;
+
+  if (Number(value_left_multiplier)) {
+    left_quadruple = quadruples[Number(value_left_multiplier)][3];
+    left_type = indexToType(left_quadruple[0]);
+    left_var_type = left_type;
+  } else {
+    left_value = value_left_multiplier.split('†')[0];
+    left_type = value_left_multiplier.split('†')[1];
+    left_var_type = left_type;
+  }
+
+  if (Number(value_right_multiplier)) {
+    right_quadruple = quadruples[Number(value_right_multiplier)][3];
+    right_type = indexToType(right_quadruple[0]);
+    right_var_type = right_type;
+  } else {
+    right_value = value_right_multiplier.split('†')[0];
+    right_type = value_right_multiplier.split('†')[1];
+    right_var_type = right_type;
+  }
+
+  if (left_type === 'var'){
+    if (findVariable(left_value) === -1){
+      alert('Variable "' + left_value + '" not defined.');
+      throw('Semantic Error');
+    } else {
+      left_index = findVariable(left_value);
+      left_var_type = indexToType(left_index);
+    }
+  } else if(left_quadruple === false) {
+    left_quadruple = left_value;
+  }
+
+  if (right_type === 'var'){
+    if (findVariable(right_value) === -1){
+      alert('Variable "' + right_value + '" not defined.');
+      throw('Semantic Error');
+    } else {
+      right_index = findVariable(right_value);
+      right_var_type = indexToType(right_index);
+    }
+  } else if(right_quadruple === false) {
+    right_quadruple = right_value;
+  }
+
+  var result_quadruple = resultType(left_var_type, right_var_type, 2);
+
+  quadruples.push(['*', left_quadruple, right_quadruple, result_quadruple]);
+
+  return '' + (quadruples.length-1);
+};
+
+Blockly.JavaScript['division'] = function(block) {
+  var value_nominator = Blockly.JavaScript.statementToCode(block, 'nominator');
+  var value_denominator = Blockly.JavaScript.statementToCode(block, 'denominator');
+
+  var left_quadruple = false;
+  var right_quadruple = false;
+
+  var left_value;
+  var left_type;
+  var left_index;
+  var left_var_type;
+
+  var right_value;
+  var right_type;
+  var right_index;
+  var right_var_type;
+
+  if (Number(value_nominator)) {
+    left_quadruple = quadruples[Number(value_nominator)][3];
+    left_type = indexToType(left_quadruple[0]);
+    left_var_type = left_type;
+  } else {
+    left_value = value_nominator.split('†')[0];
+    left_type = value_nominator.split('†')[1];
+    left_var_type = left_type;
+  }
+
+  if (Number(value_denominator)) {
+    right_quadruple = quadruples[Number(value_denominator)][3];
+    right_type = indexToType(right_quadruple[0]);
+    right_var_type = right_type;
+  } else {
+    right_value = value_denominator.split('†')[0];
+    right_type = value_denominator.split('†')[1];
+    right_var_type = right_type;
+  }
+
+  if (left_type === 'var'){
+    if (findVariable(left_value) === -1){
+      alert('Variable "' + left_value + '" not defined.');
+      throw('Semantic Error');
+    } else {
+      left_index = findVariable(left_value);
+      left_var_type = indexToType(left_index);
+    }
+  } else if(left_quadruple === false) {
+    left_quadruple = left_value;
+  }
+
+  if (right_type === 'var'){
+    if (findVariable(right_value) === -1){
+      alert('Variable "' + right_value + '" not defined.');
+      throw('Semantic Error');
+    } else {
+      right_index = findVariable(right_value);
+      right_var_type = indexToType(right_index);
+    }
+  } else if(right_quadruple === false) {
+    right_quadruple = right_value;
+  }
+
+  var result_quadruple = resultType(left_var_type, right_var_type, 3);
+
+  quadruples.push(['÷', left_quadruple, right_quadruple, result_quadruple]);
+
+  return '' + (quadruples.length-1);
 };
