04947c257c29d6fe661fa2f4ad038218f2240eef
Andres Lopez De Leon <agldeleon@gmail.com>
2015-11-16 01:42:10 -0600

    Fix multiplications with variables

--- generators/javascript/operations.js
+++ generators/javascript/operations.js
@@ -239,6 +239,7 @@ Blockly.JavaScript['multiplication'] = function(block) {
     } else {
       left_index = findVariable(left_value);
       left_var_type = indexToType(left_index);
+      left_quadruple = left_index;
     }
   } else if(left_quadruple === false) {
     left_quadruple = left_value;
@@ -251,6 +252,7 @@ Blockly.JavaScript['multiplication'] = function(block) {
     } else {
       right_index = findVariable(right_value);
       right_var_type = indexToType(right_index);
+      right_quadruple = right_index;
     }
   } else if(right_quadruple === false) {
     right_quadruple = right_value;
