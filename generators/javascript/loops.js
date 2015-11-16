f65620b2425b7074cd527f20fb6157b05f4aeff9
Andres Lopez De Leon <agldeleon@gmail.com>
2015-11-16 14:12:35 -0600

    Allow repeat loop to receive different types

--- generators/javascript/loops.js
+++ generators/javascript/loops.js
@@ -5,11 +5,43 @@ goog.provide('Blockly.JavaScript.loops');
 goog.require('Blockly.JavaScript');
 
 Blockly.JavaScript['repeat'] = function(block) {
-  var value_n_times = Blockly.JavaScript.valueToCode(block, 'n_times', Blockly.JavaScript.ORDER_ATOMIC);
-  var statements_statement = Blockly.JavaScript.statementToCode(block, 'statement');
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  return code;
+  var value_n_times = Blockly.JavaScript.statementToCode(block, 'n_times');
+
+  var left_quadruple = false;
+  var right_quadruple = false;
+
+  var left_value;
+  var left_type;
+  var left_index;
+  var left_var_type;
+
+  if (Number(value_n_times)) {
+    left_quadruple = quadruples[value_n_times][3];
+    left_type = indexToType(left_quadruple[0]);
+    left_var_type = left_type;
+  } else {
+    left_value = value_n_times.input;
+    left_type = value_n_times.type;
+    left_var_type = left_type;
+  }
+
+  if (left_type === 'var'){
+    if (findVariable(left_value) === -1){
+      alert('Variable "' + left_value + '" not defined.');
+      throw('Semantic Error');
+    } else {
+      left_index = findVariable(left_value);
+      left_var_type = indexToType(left_index);
+      left_quadruple = left_index;
+    }
+  } else if(left_quadruple === false) {
+    left_quadruple = left_value;
+  }
+
+  if(left_var_type !== 'integer') {
+    alert('Repeat loop must have an integer');
+    throw('Type Mismatch');
+  }
 };
 
 Blockly.JavaScript['while'] = function(block) {
