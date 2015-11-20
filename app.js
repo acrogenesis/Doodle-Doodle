20870d9dd1a2cf1dd25191bee38d349b004b1674
Adrian Rangel <adrian.rangel@gmail.com>
2015-11-20 09:52:05 -0600

    add shell

---
+++ app.js
@@ -0,0 +1,30 @@
+var workspace = Blockly.inject('blocklyDiv',
+    {media: '../../media/',
+     toolbox: document.getElementById('toolbox')});
+Blockly.Xml.domToWorkspace(workspace,
+    document.getElementById('startBlocks'));
+
+function resetShell(){
+  var shellBody = document.getElementById('shell-body');
+  shellBody.innerHTML = '';
+}
+
+function insertIntoShell(text){
+  var shellBody = document.getElementById('shell-body');
+  var li = document.createElement('li');
+  li.innerHTML = text;
+  shellBody.appendChild(li);
+}
+
+function showCode() {
+  resetShell();
+  // Generate JavaScript code and display it.
+  pre_compilation = true;
+  startCompilation();
+  Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+  var code = Blockly.JavaScript.workspaceToCode(workspace);
+  pre_compilation = false;
+  code = Blockly.JavaScript.workspaceToCode(workspace);
+  checkFunctionCalls();
+  alert(code);
+}
